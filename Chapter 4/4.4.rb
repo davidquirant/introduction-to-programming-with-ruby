6794aa280d396ad14d8099a3a36f7cb6a1d47057
David Quirant <daviquirant@gmail.com>
2017-02-11 07:12:20 +0000

    Chapter 4 exercises

---
+++ Chapter 4/4.4.rb
@@ -0,0 +1,7 @@
+# name.rb
+puts "What's your first name?"
+first_name = gets.chomp.to_s
+puts "and what's your last name?"
+last_name = gets.chomp.to_s
+
+puts "Your full name is: #{first_name} #{last_name}"
